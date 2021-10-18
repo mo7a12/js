b4acca4671eb0d6961a198f4c2574aaadf7fc84c
moha <sameehboshra55@gmail.com>
2021-10-18 20:03:55 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+first way to change in tag
+
+var mydiv = document.getElementById("m");
+mydiv.innerHTML = "mo7a";
+
+second by tag and index  way to change in tag
+
+var ele = document.getElementsByTagName('div');
+ele[1].innerHTML = "mo7a";
+
+three by class and index  way to change in tag
+
+var ele = document.getElementsByClassName("3");
+ele[0].innerHTML = "mo7a";
+
+four by quary 
+
+var ele = document.querySelectorAll("(div or p or h1 ).test");
+ele[0].innerHTML = "mo7a";
+*/
